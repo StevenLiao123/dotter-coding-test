9485b67eb1a48c9ae5a303f871f7dcb435cf983e
Steven Liao <stevenliao19900326@gmail.com>
2019-08-26 22:58:00 +1000

    complete table

--- src/components/Body/MembersSection/MemberSection.js
+++ src/components/Body/MembersSection/MemberSection.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ScoreTable from './ScoreTable';
-import '../../../styles/MemberSection.css';
 
-export default () => <section className="member-section px-5">
+export default () => <section className="member-section container px-5">
     <ScoreTable />
 </section>;
  
--- src/components/Body/MembersSection/ScoreTable.js
+++ src/components/Body/MembersSection/ScoreTable.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import { categories, categoryData } from '../constants';
 
-export default () => <div className="container">
-<h4>Member (3)</h4>         
-<table className="table table-hover" data-toggle="table" data-sort-name="date" data-sort-order="desc">
-  <thead>
-    <tr>
-      <th>Firstname</th>
-      <th>Lastname</th>
-      <th>{categoryData[categories.PRODUCT_QUALITY]}</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>John</td>
-      <td>Doe</td>
-      <td>john@example.com</td>
-    </tr>
-    <tr>
-      <td>Mary</td>
-      <td>Moe</td>
-      <td>mary@example.com</td>
-    </tr>
-    <tr>
-      <td>July</td>
-      <td>Dooley</td>
-      <td>july@example.com</td>
-    </tr>
-  </tbody>
-</table>
-
-</div>
+export default () => <React.Fragment>
+  <h6>Member (3)</h6>
+  <div className="table-responsive">
+    <table className="table table-hover" data-toggle="table" data-sort-name="date" data-sort-order="desc">
+      <thead>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>{categoryData[categories.PRODUCT_QUALITY]}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>john@example.com</td>
+        </tr>
+        <tr>
+          <td>Mary</td>
+          <td>Moe</td>
+          <td>mary@example.com</td>
+        </tr>
+        <tr>
+          <td>July</td>
+          <td>Dooley</td>
+          <td>july@example.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</React.Fragment>;
