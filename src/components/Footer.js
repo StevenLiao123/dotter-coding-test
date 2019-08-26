7723c556e798805e52269116a619e8e7ea63eb7f
Steven Liao <stevenliao19900326@gmail.com>
2019-08-26 21:36:17 +1000

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default () => <footer id="sticky-footer" className="py-4 bg-dark text-white-50 ft-bg">
-    <div className="container text-center">
-        <small>Copyright &copy; Your Website</small>
+export default () => <footer id="sticky-footer" className="py-4 bg-dark text-white-50">
+    <div className="container">
+        <small> &copy; 2019 Dotter. All rights reserved.</small>
     </div>
 </footer>;
