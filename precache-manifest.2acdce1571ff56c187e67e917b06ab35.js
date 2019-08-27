1d0bcb600e3f7134e8420782b79a6504c09269c3
Steven Liao <stevenliao19900326@gmail.com>
2019-08-27 11:13:46 +1000

    Updates

---
+++ precache-manifest.2acdce1571ff56c187e67e917b06ab35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb96c5628dd1b4415858bd13a0cc6fb2",
+    "url": "/dotter-coding-test/index.html"
+  },
+  {
+    "revision": "9d7d4ee1884f497be754",
+    "url": "/dotter-coding-test/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e9e39efa461c6b06ad33",
+    "url": "/dotter-coding-test/static/css/main.04973020.chunk.css"
+  },
+  {
+    "revision": "9d7d4ee1884f497be754",
+    "url": "/dotter-coding-test/static/js/2.247dea82.chunk.js"
+  },
+  {
+    "revision": "e9e39efa461c6b06ad33",
+    "url": "/dotter-coding-test/static/js/main.82b5a1b6.chunk.js"
+  },
+  {
+    "revision": "d8bd4eddc0a0ddd917b1",
+    "url": "/dotter-coding-test/static/js/runtime~main.1cc39468.js"
+  }
+]);
