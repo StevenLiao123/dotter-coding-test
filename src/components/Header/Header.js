da0a80662bde66781d4f2d10458042b9349e697b
XIAOHUI LIAO <182272546@qq.com>
2019-09-19 23:15:46 +1000

    Updated the url of images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 
 export default () => <nav className="navbar navbar-expand-md navbar-dark bg-dark dotterNav px-5 py-3">
-    <a className="navbar-brand" href="/"><img src="/assets/images/dotter-logo-2x.png" className="img-fluid" alt="dotter-logo" width="100" height="100" /></a>
+    <a className="navbar-brand" href="/"><img src="/portfolio/assets/images/logo1.png" className="img-fluid" alt="dotter-logo" width="100" height="100" /></a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
     </button>
@@ -14,7 +14,7 @@ export default () => <nav className="navbar navbar-expand-md navbar-dark bg-dark
             <li className="nav-item">
                 <a className="nav-link text-white" href="/">
                     <span className="text-white">Hello Sam</span>
-                    <img src="/assets/images/user-logo.jpg" className="rounded-circle ml-2" alt="Cinque Terre" width="30" height="30" />
+                    <img src="/portfolio/assets/images/logo2.jpg" className="rounded-circle ml-2" alt="Cinque Terre" width="30" height="30" />
                 </a>
             </li>
         </ul>
